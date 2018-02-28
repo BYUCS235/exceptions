4da5e85c7d7873511b8de9d3c24af3a044c9cf88
Mark Clement <clement@cs.byu.edu>
2018-02-27 22:08:40 -0700

    Update except2.cpp

--- except2.cpp
+++ except2.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 int main()
 {
-    stringstream ss("Speedy R P");
-    string name, type;
+	stringstream ss("Speedy R P");
+	string name, type;
 	int speed;
 	ss.exceptions(ios_base::failbit);
-	
+
 	try {
 	    ss >> name >> type >> speed;
 	} 
